29863533d181abc8959ade1d13e3364726c80afb
Matheus Passatuto <matheus.passatuto@bkbank.com.br>
2024-08-02 13:26:41 -0300

    Revert "Criando a classe Posição#"
    This reverts commit 20db3aa896223cf854444633282c13a99a4b2d7d.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Tabuleiro;
 
 namespace xadrez_console
 {
@@ -7,13 +6,7 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
-
-            Console.ReadLine();
         }
     }
 }
