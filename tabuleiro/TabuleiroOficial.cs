eb04cc785ee1145388232e6bec48d5a9bb4abde5
Matheus Passatuto <matheus.passatuto@bkbank.com.br>
2024-08-02 18:05:05 -0300

    Fazendos os primeiros testes de movimentacao

--- tabuleiro/TabuleiroOficial.cs
+++ tabuleiro/TabuleiroOficial.cs
@@ -36,6 +36,18 @@ namespace Tabuleiro
             p.posicao = pos;
         }
 
+        public Peca retirarPeca(Posicao pos)
+        {
+            if(peca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null;
+            pecas[pos.linha, pos.coluna] = null;
+            return aux;
+        }
+
         public bool existePeca(Posicao pos)
         {
             validarPosicao(pos);
