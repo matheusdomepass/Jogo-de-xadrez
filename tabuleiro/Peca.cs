f32c0dd2ebf0a6fbddcce307771d5cef31d9d4dd
Matheus Passatuto <matheus.passatuto@bkbank.com.br>
2024-08-02 13:51:08 -0300

    Começando a implementar o tabuleiro.

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public TabuleiroOficial tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, TabuleiroOficial tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.tab = tab;
+            this.qteMovimentos = 0;
+        }
+    }
+}
---
+++ tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace Tabuleiro
+{
+    class TabuleiroOficial
+    {
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public TabuleiroOficial(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
