10c14fcbcfed401ac5b1cdc576cddb3961a264e7
Matheus Passatuto <matheus.passatuto@bkbank.com.br>
2024-08-07 09:49:27 -0300

    Implementando a regra de XEQUE

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -19,7 +19,10 @@
         {
             qteMovimentos++;
         }
-
+        public void decrementarQteMovimentos()
+        {
+            qteMovimentos--;
+        }
         public bool existeMovimentosPossives()
         {
             bool[,] mat = movimentosPossiveis();
